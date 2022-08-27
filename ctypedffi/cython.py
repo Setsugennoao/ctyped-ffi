f2d525c2379f0fee1d0376bb654d5a61521a2fe5
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-08-27 23:54:36 +0200

    CythonModule: more meaningful errors

--- ctypedffi/cython.py
+++ ctypedffi/cython.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from types import ModuleType
-from typing import Any, Mapping
+from typing import Any, Mapping, NoReturn
 
 from .libs import PyCapsule
 from .struct import Struct, StructMeta
@@ -12,44 +12,65 @@ __all__ = [
     'CythonModuleMeta', 'CythonModule'
 ]
 
+_DUMMYMODULE = object()
+
 
 class CythonModuleMetaDict(MetaClassDictBase):
-    def __init__(self, module: ModuleType) -> None:
+    def __init__(self, cls_name: str, module: ModuleType | None) -> None:
+        self.cls_name = cls_name
         self.module = module
-        self.capsules = module.__pyx_capi__
+
+        if module:
+            self.capsules = module.__pyx_capi__
+        else:
+            self.capsules = dict[str, Any]()
 
         super().__init__(module=self.module, capsules=self.capsules, __slots__=[], _fields_=[])
 
     def _setitem_(self, name: str, value: Any, /) -> None:
         if callable(value):
-            try:
-                capsule = self.capsules[name]
-            except KeyError as e:
-                raise AttributeError(name) from e
+            if self.module is None:
+                value = self._raise_module_unavailable
+            else:
+                try:
+                    capsule = self.capsules[name]
+                except KeyError as e:
+                    raise AttributeError(name) from e
 
-            func_type = as_cfunc(value, name)
+                func_type = as_cfunc(value, name)
 
-            mangled_name = PyCapsule.GetName(capsule)
-            capsule_ptr = PyCapsule.GetPointer(capsule, mangled_name)
+                mangled_name = PyCapsule.GetName(capsule)
+                capsule_ptr = PyCapsule.GetPointer(capsule, mangled_name)
 
-            value = func_type(capsule_ptr)
+                value = func_type(capsule_ptr)
 
         return dict.__setitem__(self, name, value)
+
+    def _raise_module_unavailable(self, *args: Any, **kwargs: Any) -> NoReturn:
+        raise ModuleNotFoundError(
+            f'{self.cls_name}: The cython module is unavailable!'
+        )
 
 
 class CythonModuleMeta(StructMeta):
     @classmethod
     def __prepare__(metacls, name: str, bases: tuple[type, ...], /, **kwargs: Any) -> Mapping[str, object]:
-        if 'module' in kwargs:
-            module = kwargs.pop('module')
+        if 'module' not in kwargs:
+            raise ValueError(
+                'CythonModule: You must specify a module with subclass kwargs!'
+            )
 
-            if module is not None:
-                if not hasattr(module, '__pyx_capi__'):
-                    raise ValueError("Not a cython module.")
+        module = kwargs.pop('module')
 
-                return CythonModuleMetaDict(module)
+        if module is _DUMMYMODULE:
+            return dict[str, Any]()
 
-        return dict[str, Any]()
+        if module is not None and not hasattr(module, '__pyx_capi__'):
+            raise ValueError(
+                'CythonModule: Passed module isn\'t a cython module!'
+            )
+
+        return CythonModuleMetaDict(name, module)
 
     def __new__(
         cls: type[Self], name: str, bases: tuple[type, ...], namespace: dict[str, Any], /, **kwargs: Any
@@ -57,5 +78,5 @@ class CythonModuleMeta(StructMeta):
         return super().__new__(cls, name, bases, namespace)  # type: ignore
 
 
-class CythonModule(Struct, metaclass=CythonModuleMeta, module=None):
+class CythonModule(Struct, metaclass=CythonModuleMeta, module=_DUMMYMODULE):
     ...
