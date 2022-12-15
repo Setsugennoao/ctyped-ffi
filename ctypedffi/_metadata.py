530ad57c4e061c448314a8d3749e325512fb1784
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-12-16 00:04:27 +0100

    Bump major version

--- ctypedffi/_metadata.py
+++ ctypedffi/_metadata.py
@@ -1,6 +1,6 @@
 """Wrapper around ctypes, for creating typed foreign function libraries"""
 
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 
 __author__ = 'Setsugen no ao <setsugen@setsugen.dev>'
 __maintainer__ = __author__
