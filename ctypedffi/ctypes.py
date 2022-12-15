03af809eb8902c53577d78d6d20a2198ac2f4b36
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-12-15 23:05:29 +0100

    Add `memmove`

--- ctypedffi/ctypes.py
+++ ctypedffi/ctypes.py
@@ -3,10 +3,9 @@ import ctypes
 from ctypes import (
     CFUNCTYPE, POINTER, PYFUNCTYPE, Array, Structure, _SimpleCData, addressof, c_char, c_char_p, c_double, c_float,
     c_int, c_int8, c_int16, c_int32, c_int64, c_longlong, c_short, c_size_t, c_ssize_t, c_ubyte, c_uint, c_uint8,
-    c_uint16, c_uint32, c_uint64, c_ulonglong, c_ushort, c_void_p, memmove
+    c_uint16, c_uint32, c_uint64, c_ulonglong, c_ushort, c_void_p, memmove, pythonapi, sizeof, string_at
 )
 from ctypes import py_object as py_object_t
-from ctypes import pythonapi, sizeof, string_at
 from functools import lru_cache
 from types import FunctionType, MappingProxyType
 from typing import TYPE_CHECKING, Any, cast
@@ -41,6 +40,8 @@ __all__ = [
     'None_ptr',
 
     'get_stgdict_of_type', 'make_callback_returnable',
+
+    'memmove'
 ]
 
 
